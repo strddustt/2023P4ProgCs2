facb842a9b89ee276f3488a937039b3eead04133
Aidan <38162@ma-web.nl>
2024-05-13 12:28:52 +0200

    opdracht 4

--- ConsoleMonGame/ConsoleApp1/Program.cs
+++ ConsoleMonGame/ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            TestConsoleMonFunctions();
+            TestSkillFunctions();
         }
         static void TestConsoleMonFunctions()
         {
@@ -17,6 +17,22 @@
             Console.WriteLine(mon.health == -100);
 
             Console.WriteLine(mon.energy == -20);
+        }
+        static void TestSkillFunctions()
+        {
+            Console.WriteLine("TestSkillFunctions");
+            ConsoleMon casterMon = new ConsoleMon();
+            ConsoleMon targetMon = new ConsoleMon();
+            Skill skill = new Skill()
+            {
+                damage = 100,
+                energyCost = 20,
+            };
+            skill.UseOn(targetMon, casterMon);
+
+            Console.WriteLine(targetMon.health == -100);
+
+            Console.WriteLine(casterMon.energy == -20);
         }
     }
 }
--- ConsoleMonGame/ConsoleApp1/Skill.cs
+++ ConsoleMonGame/ConsoleApp1/Skill.cs
@@ -11,5 +11,12 @@ namespace ConsoleApp1
         internal int damage;
         internal int energyCost;
         internal string name;
+        
+
+        internal void UseOn(ConsoleMon target, ConsoleMon caster)
+        { 
+            caster.DepleteEnergy(energyCost);
+            target.TakeDamage(damage);
+        }
     }
 }
